5ce59b8052fa5de6a0430a753ad32fb08e8e6bce
Del Pinto Daniele <d.delpinto@almawave.it>
2019-10-10 14:05:04 +0200

    riordinamento codice

--- index.js
+++ index.js
@@ -10,27 +10,40 @@ const label = (s) => { if (s == null || s.length == 0) { return 'undefined'; } e
 
 const engine = newEngine();
 
-async function handleData() {
+//scrivo il grafo nel formato cytoscape su file
+async function writeToFile(nodes, edges, compounds) {
 
-  //carico la query SPARQL
-  var query = '';
-  var sources = [];
-  try {
-      objQuery =  JSON.parse( _fs.readFileSync('dbpedia.sparql.json', 'utf8') );
-      //prendo la query per le classi e le object property
-      query = objQuery.query.find(x => x.name === 'classes').value.join("\n");
-      sources = objQuery.sources;
+  let optObj = {};
 
-      console.dir(sources, {depth:3});
-      console.log('-------------------------------------');
-      console.log(query);
-      console.log('-------------------------------------');
+  _fs.readFile('options.json', 'utf-8', (err, data) => {
+    if (err) throw err;
 
-  } catch(e) {
-      console.log('Error reading query:', e.stack);
-  }
+    var graphObj = { id: 1, name: 'nome', graph: [], options: {} }
+    var graphVectorObj = { graphs: [] };
+    optObj = JSON.parse(data);
 
-  function streamToString (stream) {
+    console.log("nodes :" + nodes.length + ", edges:" + edges.length + ", compounds:" + compounds.length);
+
+    graphObj.graph = [...nodes, ...edges, ...compounds];
+    graphObj.options = optObj;
+    graphVectorObj.graphs.push(graphObj);
+
+    console.log("created :" + graphObj.graph.length)
+
+    _fs.writeFile('graph-db.json', JSON.stringify(graphVectorObj, null, 2), 'utf-8', (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+
+  });
+
+}
+
+
+
+async function executeQuery(query, sources) {
+
+  function streamToString(stream) {
     const chunks = []
     return new Promise((resolve, reject) => {
       stream.on('data', chunk => chunks.push(chunk))
@@ -40,27 +53,53 @@ async function handleData() {
   }
 
   const result = await engine.query(query, { sources });
-  const readable =  (await engine.resultToString(result, 'application/sparql-results+json', result.context)).data;
+  const readable = (await engine.resultToString(result, 'application/sparql-results+json', result.context)).data;
   const risultato = await streamToString(readable);
-  objResult =  JSON.parse( risultato );
+  objResult = JSON.parse(risultato);
 
   return objResult;
 
 } //END async function handleData()
 
-  console.log('start');
+//stampa sulla console la query
+function logQuery(query, sources) {
 
-  handleData().then(
+  console.log("");
+  console.dir(sources, { depth: 3 });
+  console.log('-------------------------------------');
+  console.log(query);
+  console.log('-------------------------------------');
+
+}
+
+/**
+ * INIZIO PROCESSO
+ */
+console.log('start');
+
+//carico le query SPARQL
+var query = '';
+var sources = [];
+try {
+  objQuery = JSON.parse(_fs.readFileSync('veneto.sparql.json', 'utf8'));
+} catch (e) {
+  console.log('Error reading query:', e.stack);
+}
+
+//prendo la query per le classi e le object property
+query = objQuery.query.find(x => x.name === 'classes').value.join("\n");
+sources = objQuery.sources;
+logQuery(query, sources);
+executeQuery(query, sources).then(
   (data) => {
+    console.log("QUERY OK");
+    console.log("resultSet rows: " + data.results.bindings.length);
 
-    //console.dir(data, {depth:5});
-    console.log(data.results.bindings.length);
-
-    function mapToNode (o, nc, cl, lb) {
+    function mapToNode(o, nc, cl, lb) {
       _lb = "";
-      eval ('if(o.'+lb+') _lb=o.'+lb+'.value');
+      eval('if(o.' + lb + ') _lb=o.' + lb + '.value');
       _cl = "";
-      eval ('if(o.'+cl+') _cl=o.'+cl+'.value');
+      eval('if(o.' + cl + ') _cl=o.' + cl + '.value');
 
       d = {
         id: "n" + nc,
@@ -68,17 +107,16 @@ async function handleData() {
         type: 'node',
         label: _lb,
         uri: _cl,
-        class: eval('o.'+cl+'.type==="uri"?"Class":"_blank"')
+        class: eval('o.' + cl + '.type==="uri"?"Class":"_blank"')
       };
-      return {data: d, position: p, group: 'nodes', removed : false, selected: false, selectable: true, locked: false, grabbable: true, classes: ''};
+      return { data: d, position: p, group: 'nodes', removed: false, selected: false, selectable: true, locked: false, grabbable: true, classes: '' };
     }
 
-    //TODO
     function mapToEdge(o, nt, np, lb, pr) {
       _lb = "";
-      eval ('if(o.'+lb+') _lb=o.'+lb+'.value');
+      eval('if(o.' + lb + ') _lb=o.' + lb + '.value');
       _pr = "";
-      eval ('if(o.'+pr+') _cl=o.'+pr+'.value');
+      eval('if(o.' + pr + ') _cl=o.' + pr + '.value');
       _cl = "";
       if (o.type) _cl = o.type.value;
 
@@ -92,106 +130,75 @@ async function handleData() {
         source: nt.find(x => x.data.uri === o.class_a.value).data.id,
         target: nt.find(x => x.data.uri === o.class_b.value).data.id
       };
-      return {data: d, position: p, group: 'edges', removed : false, selected: false, selectable: true, locked: false, grabbable: true, classes: ''};
+      return { data: d, position: p, group: 'edges', removed: false, selected: false, selectable: true, locked: false, grabbable: true, classes: '' };
     }
 
-    console.log("QUERY OK");
     var nc = 1;
     var np = 1;
 
     p = { x: 0, y: 0 };
-    ca = _lh.map(data.results.bindings, (o) => mapToNode(o,nc++, 'class_a', 'class_a_label'));
-    cb = _lh.map(data.results.bindings, (o) => mapToNode(o,nc++, 'class_b', 'class_b_label'));
+    ca = _lh.map(data.results.bindings, (o) => mapToNode(o, nc++, 'class_a', 'class_a_label'));
+    cb = _lh.map(data.results.bindings, (o) => mapToNode(o, nc++, 'class_b', 'class_b_label'));
 
     //unisco lasse_a e classe_b e tolgo i doppioni
-    nt = _lh.uniqBy(_lh.concat(ca,cb),'data.uri');
+    nt = _lh.uniqBy(_lh.concat(ca, cb), 'data.uri');
 
     //CREAZIONE COMPOUND
-      function extractPrefix(str) {
+    function extractPrefix(str) {
 
-        lastSlash = str.lastIndexOf('/');
-        lastHash = str.lastIndexOf('#');
-        if (lastHash != -1 && lastSlash != -1)
-          if (lastHash > lastSlash)
-            if (lastHash - lastSlash == 1)
-              return str.substring(0, lastSlash + 1)
-            else return str.substring(0, lastHash + 1)
-          else return str.substring(0, lastSlash + 1)
-        if (lastHash != -1) return str.substring(0, lastHash + 1)
-        else
-          if (lastSlash != -1) return str.substring(0, lastSlash + 1);
-          else return "_blank";
+      lastSlash = str.lastIndexOf('/');
+      lastHash = str.lastIndexOf('#');
+      if (lastHash != -1 && lastSlash != -1)
+        if (lastHash > lastSlash)
+          if (lastHash - lastSlash == 1)
+            return str.substring(0, lastSlash + 1)
+          else return str.substring(0, lastHash + 1)
+        else return str.substring(0, lastSlash + 1)
+      if (lastHash != -1) return str.substring(0, lastHash + 1)
+      else
+        if (lastSlash != -1) return str.substring(0, lastSlash + 1);
+        else return "_blank";
 
+    }
+
+    cc = []; // vettore dei compound
+    nc = 1; //nuimero dei compound
+    lastPr = ""; //parte comune dell'uri
+
+    //lavoro sulle uri ordinate
+    sorted_nt = _lh.sortBy(nt, ["data.uri"]);
+    sorted_nt.forEach((element, index, array) => {
+
+      pr = extractPrefix(element.data.uri);
+      if (lastPr === "") {
+        lastPr = pr;
+        //inserisco il primo compound
+        cc.push({ group: 'nodes', data: { id: "c" + nc++, label: pr, type: "node", class: 'compound' }, selectable: false, grabbable: false });
       }
 
-      cc = []; // vettore dei compound
-      nc = 1; //nuimero dei compound
-      lastPr = ""; //parte comune dell'uri
+      if (pr === lastPr) {
+        element.data.parent = "c" + (nc - 1);
+      } else {
+        //console.log(lastPr + " CLOSED ===================================================\n");
 
-      //lavoro sulle uri ordinate
-      sorted_nt = _lh.sortBy(nt, ["data.uri"]);
-      sorted_nt.forEach((element, index, array) => {
+        //creo un nuovo compound
+        cc.push({ group: 'nodes', data: { id: "c" + nc++, label: pr, type: "node", class: 'compound' }, selectable: false, grabbable: false });
+        //aggiungo l'elemento corrente al compound creato
+        element.data.parent = "c" + (nc - 1);
 
-        pr = extractPrefix(element.data.uri);
-        if (lastPr === "") {
-          lastPr = pr;
-          //inserisco il primo compound
-          cc.push({ group: 'nodes', data: { id: "c" + nc++, label: pr, type: "node", class: 'compound' }, selectable: false, grabbable: false });
+        if (lastPr.startsWith(pr)) {
+          cc[nc - 2].data.parent = cc[nc - 3].data.id;
         }
-
-        if (pr === lastPr) {
-          element.data.parent = "c" + (nc - 1);
-        } else {
-          console.log(lastPr + " CLOSED ===================================================\n");
-
-          //creo un nuovo compound
-          cc.push({ group: 'nodes', data: { id: "c" + nc++, label: pr, type: "node", class: 'compound' }, selectable: false, grabbable: false });
-          //aggiungo l'elemento corrente al compound creato
-          element.data.parent = "c" + (nc - 1);
-
-          if (lastPr.startsWith(pr)) {
-            cc[nc - 2].data.parent = cc[nc - 3].data.id;
-          }
-          lastPr = pr;
-        }
-      });
+        lastPr = pr;
+      }
+    });
     //FINE CREAZIONE COMPOUND
 
-    /*
-    pp = _lh.map(data.results.bindings, (o) => { return {id : np++, uri : o.objprop.value, label : o.prop_label.value, type : o.type.value, from: o.class_a.value, to: o.class_b.value } });
-    pt = _lh.map(pp, (o) => {
-      d = { id: "e"+o.id, weight: 2, type: 'edge', label: o.label, uri:o.uri, class: o.type, source: nt.find(x => x.data.uri === o.from).data.id, target: nt.find(x => x.data.uri === o.to).data.id };
-      return {data: d, position: p, group: 'edges', removed : false, selected: false, selectable: true, locked: false, grabbable: true, classes: ''};
-    });
-    */
+    //mappo gli archi
     pt = _lh.map(data.results.bindings, (o) => mapToEdge(o, nt, np++, 'objprop', 'prop_label'));
 
-
-    //SALVO SU FILE JSON
-
-    let optObj={};
-
-    _fs.readFile('options.json', 'utf-8',  (err, data) => {
-      if (err) throw err;
-
-      var graphObj = {id: 1, name: 'nome', graph: [], options : {}}
-      var graphVectorObj = { graphs: [] };
-      optObj = JSON.parse(data);
-
-      console.log("nodes :"+ sorted_nt.length + ", edges:"+pt.length+ ", compounds:"+cc.length);
-
-      graphObj.graph = [...sorted_nt, ...pt, ...cc];
-      graphObj.options = optObj;
-      graphVectorObj.graphs.push(graphObj);
-
-      console.log("created :"+ graphObj.graph.length)
-
-      _fs.writeFile('graph-db.json', JSON.stringify(graphVectorObj,null,2) , 'utf-8', (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
-      });
-
-    });
+    //scrivo su disco il grafo
+    writeToFile(sorted_nt, pt, cc);
 
   }).catch(
     (error) => {
@@ -199,5 +206,5 @@ async function handleData() {
       console.error(error);
     }).finally(
       (data) => {
-        console.log('end');
+        //console.log('end');
       });
